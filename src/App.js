7f8ac6c7f3655e0917f02dff30c608a9aad55173
Ramzi <Ouerfelliramsey@gmail.com>
2023-11-10 16:23:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Class-based component
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Ramzi Ouerfelli',
+      imgSrc: 'https://scontent.ftun1-2.fna.fbcdn.net/v/t39.30808-6/384105790_189002427553539_866122583979115565_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=BAu17VL4VsoAX-KvlRi&_nc_ht=scontent.ftun1-2.fna&oh=00_AfByqXvvRBcVKTh4aT0MYeaFumuzup7Y9SQYpPD2mqS_Tw&oe=65533EFA',
+      bio: 'Im rapper And Devolopper',
+      
+    },
+    show: true,
+    mountTime: new Date(),
+  };
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.forceUpdate(); // Update the component 
+    }, 1000); // Update the time every second
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    const { person, show, mountTime } = this.state;
+
+    return (
+      <div className='myCard'>
+        {show && (
+          <div className='myobject'>
+            <h2 style={{color: "#3C4B5C",}}>{person.fullName}</h2>
+            <img style={{width:"20rem", borderRadius:"10rem"}} src={person.imgSrc} alt="Profile" />
+            <p style={{fontSize:"20px"}}> <br /> {person.bio}</p>
+          </div>
+        )}
+        <button className='button' style={{verticalAlign:"middle"}} onClick={this.toggleShow}>CLICK</button>
+        <p>{Math.floor((new Date() - mountTime) / 1000)}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
